01c4eb8a3f8a7a50794e1b5f469890cebef68a98
Oliver Lindemann <lindemann.oliver@gmx.de>
2019-11-14 19:00:53 +0100

    Methoden zum Hinzufuegen, Holen und Entfernen von Attributen erstellt

--- Webprojekt/src/de/hsharz/empfehlungssystem/servlet/Session.java
+++ Webprojekt/src/de/hsharz/empfehlungssystem/servlet/Session.java
@@ -8,6 +8,7 @@ import de.hsharz.empfehlungssystem.beans.User;
 public class Session {
 
 	public static final String ATTRIBUTE_LOGGED_IN_USER = "loggedInUser";
+	public static final String ATTRIBUTE_REGISTERING_USER = "registeringUser";
 
 	private static Map<String, Object> attributes = new HashMap<>();
 
@@ -22,7 +23,7 @@ public class Session {
 	 * @param loggedInUser Benutzer des aktuell angemeldeten Benutzers
 	 */
 	public static void storeLoggedInUser(User loggedInUser) {
-		attributes.put(ATTRIBUTE_LOGGED_IN_USER, loggedInUser);
+		store(ATTRIBUTE_LOGGED_IN_USER, loggedInUser);
 	}
 
 	/**
@@ -33,14 +34,45 @@ public class Session {
 	 *         vorhanden ist
 	 */
 	public static User getLoggedInUser() {
-		return (User) attributes.get(ATTRIBUTE_LOGGED_IN_USER);
+		return get(ATTRIBUTE_LOGGED_IN_USER);
 	}
 
 	/**
 	 * Entfernt den aktuell gespeicherten Benutzer aus der Attribute-Map
 	 */
 	public static void removeLoggedInUser() {
-		attributes.remove(ATTRIBUTE_LOGGED_IN_USER);
+		remove(ATTRIBUTE_LOGGED_IN_USER);
 	}
 
+	/**
+	 * Speichert das gegebene Object unter dem gegebenen Key ab.
+	 * 
+	 * @param key   Key unter dem das gegebene Objekt abgespeichert werden soll
+	 * @param value Objekt, das unter dem gegebenen Key abgespeichert werden soll
+	 */
+	public static void store(String key, Object value) {
+		attributes.put(key, value);
+	}
+
+	/**
+	 * 
+	 * @param <T>
+	 * @param key
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public static <T> T get(String key) {
+		return (T) attributes.get(key);
+	}
+
+	/**
+	 * 
+	 * @param <T>
+	 * @param key
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public static <T> T remove(String key) {
+		return (T) attributes.get(key);
+	}
 }
