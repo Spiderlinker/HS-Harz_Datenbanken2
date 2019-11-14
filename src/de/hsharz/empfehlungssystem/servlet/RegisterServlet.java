be48affcb33efc9041a320d20ff0be66b8d31f09
Spiderlinker <support@spiderlinker.de>
2019-11-14 09:18:13 +0100

    Unterscheidung von beiden RegisterPages hinzugefügt

--- src/de/hsharz/empfehlungssystem/servlet/RegisterServlet.java
+++ src/de/hsharz/empfehlungssystem/servlet/RegisterServlet.java
@@ -17,26 +17,27 @@ import de.hsharz.empfehlungssystem.database.DatabaseAdapter;
 /**
  * Servlet implementation class RegisterServlet
  */
-@WebServlet(description = "Register", urlPatterns = { "/Register", "/RegisterPage", "/RegisterServlet", "/Genre" })
+@WebServlet(description = "Register", urlPatterns = { "/Register", "/RegisterPage", "/RegisterServlet", })
 public class RegisterServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
 	private static final String CHECKBOX_WITH_LABEL = "<input type=\"checkbox\" hidden=\"hidden\" id=\"%s\" name=\"%s\" /><label for=\"%s\">%s</label>";
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public RegisterServlet() {
-        super();
-    }
-    
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public RegisterServlet() {
+		super();
+	}
+
 	public String getGenres() {
 
 		List<String> genreNames = new ArrayList<>();
-		try {
-			genreNames = DatabaseAdapter.getAllGenreNames();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+//		try {
+//			genreNames = DatabaseAdapter.getAllGenreNames();
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		}
 
 		StringBuilder builder = new StringBuilder();
 		for (String genre : genreNames) {
@@ -44,23 +45,51 @@ public class RegisterServlet extends HttpServlet {
 		}
 		return builder.toString();
 	}
-    
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/jsps/Register.jsp");
 		dispatcher.forward(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-		request.getParameter("");
-		
+		String registerPage = request.getParameter("registerPage");
+
+		if ("1".equals(registerPage)) {
+			RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/jsps/Register2.jsp");
+			dispatcher.forward(request, response);
+			return;
+		} else if ("2".equals(registerPage)) {
+
+		} else {
+
+		}
+
+		System.out.println("Register do post...");
+		System.out.println(request.getParameter("firstname"));
+		System.out.println(request.getAttribute("firstname"));
+		System.out.println(request.getParameter("lastname"));
+		System.out.println(request.getAttribute("lastname"));
+
+		String isSelectGenrePage = request.getParameter("selectGenrePage");
+		if (isSelectGenrePage == null) {
+			System.out.println("Forwarding to page 2");
+
+		}
+
+		System.out.println(request.getParameter("firstname"));
+		System.out.println(request.getParameter("lastname"));
+		// Registrierung abschlieﬂen
 	}
 
 }
