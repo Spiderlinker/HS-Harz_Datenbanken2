e06b8c19e402bed702204ec0b0bfa0f76feb9f9d
Spiderlinker <support@spiderlinker.de>
2019-11-14 09:17:51 +0100

    Javadoc hinzugefügt

--- src/de/hsharz/empfehlungssystem/utils/SessionUtils.java
+++ src/de/hsharz/empfehlungssystem/utils/SessionUtils.java
@@ -2,8 +2,6 @@ package de.hsharz.empfehlungssystem.utils;
 
 import java.io.IOException;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -12,14 +10,27 @@ import de.hsharz.empfehlungssystem.servlet.Session;
 
 public class SessionUtils {
 
-	public static boolean redirectToLoginPageIfNotLoggedIn(HttpServlet servlet, HttpServletRequest request,
-			HttpServletResponse response) throws IOException, ServletException {
+	private static final String LOGIN_PAGE_PATH = "/Login";
+
+	/**
+	 * Leitet den User auf die Login-Page weiter, sofern dieser nicht im System
+	 * angemeldet ist. Falls der Benutzer bereits angemeldet ist, so wird der User
+	 * als Attribut in den gegebenen request geschrieben.
+	 * 
+	 * @param request  HttpServletRequest des Users
+	 * @param response HttpServletResponse für den User
+	 * @return ob der Benutzer auf die Login-Page weitergeleitet wurde
+	 * @throws IOException Fehler beim Weiterleiten
+	 */
+	public static boolean redirectToLoginPageIfNotLoggedIn(HttpServletRequest request, HttpServletResponse response)
+			throws IOException {
 		User loggedInUser = Session.getLoggedInUser();
 
 		if (loggedInUser == null) {
-			response.sendRedirect(request.getContextPath() + "/Login");
+			response.sendRedirect(request.getContextPath() + LOGIN_PAGE_PATH);
 			return true;
 		}
+
 		request.setAttribute("user", loggedInUser);
 		return false;
 	}
