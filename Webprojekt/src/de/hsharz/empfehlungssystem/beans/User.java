a5825856ef8bfa08faec2af8efda553ff4485905
Oliver Lindemann <lindemann.oliver@gmx.de>
2019-12-18 17:06:48 +0100

    Preferences 1-3 in User eingefuegt

--- Webprojekt/src/de/hsharz/empfehlungssystem/beans/User.java
+++ Webprojekt/src/de/hsharz/empfehlungssystem/beans/User.java
@@ -20,6 +20,10 @@ public class User {
 	private String password;
 	
 	private String birthday;
+
+	private String preference1;
+	private String preference2;
+	private String preference3;
 	
 	
 	public User() {
@@ -136,6 +140,30 @@ public class User {
 
 	public void setCountryShort(String countryShort) {
 		this.countryShort = countryShort;
+	}
+
+	public String getPreference3() {
+		return preference3;
+	}
+
+	public void setPreference3(String preference3) {
+		this.preference3 = preference3;
+	}
+
+	public String getPreference2() {
+		return preference2;
+	}
+
+	public void setPreference2(String preference2) {
+		this.preference2 = preference2;
+	}
+
+	public String getPreference1() {
+		return preference1;
+	}
+
+	public void setPreference1(String preference1) {
+		this.preference1 = preference1;
 	}
 
 
